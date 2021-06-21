18e0ead4902a533a581f3888ba7956c5492cb641
blueskyarea <blueskyareahm@gmail.com>
2021-06-21 20:13:54 +0900

    recipe

---
+++ recipe/recipe_import.py
@@ -0,0 +1,14 @@
+# Simple import
+import math
+print(math.pi)
+
+# Part of import
+from math import pi
+print(pi)
+
+# Import with named
+import math as m
+print(m.pi)
+
+from math import pi as p
+print(p)
---
+++ recipe/recipe_print.py
@@ -0,0 +1,19 @@
+# Output character
+print("abc")
+
+# Output variable
+test = "def"
+num = 77
+l = [100, 200, 300]
+print(test, num, l)
+
+# Specify output delimiter
+x = 3
+y = 5
+z = 7
+print(x, y, z, sep=',')
+
+# Not change line after print
+print("Not ", end="")
+print("change ", end="")
+print("line")
